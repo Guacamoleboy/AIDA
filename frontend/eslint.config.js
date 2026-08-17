3f2306c63d80cf0244493bc5a0eb1b848a87effc
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-08-17 18:40:09 +0200

    PR Template test 1

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,7 @@
+
+// Enables TypeScript checks for JavaScript files (.js)
+// @ts-check
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -5,18 +9,20 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+export default defineConfig(
+  [
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs.flat.recommended,
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        globals: globals.browser,
+      },
     },
-  },
-])
+  ]
+)
