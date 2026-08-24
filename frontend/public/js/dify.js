71061aa9ce12e1905cfa47ec20bbaf1e2e895c38
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-08-24 19:21:57 +0200

    Final Dify Adjustments + Addition to Portfolio Deployment Site

---
+++ frontend/public/js/dify.js
@@ -0,0 +1,15 @@
+window.difyChatbotConfig = {
+    token: 'Biy8n9oNdzKDhB6b',
+    baseUrl: 'https://udify.app',
+    inputs: {},
+    systemVariables: {},
+    userVariables: {},
+}
+
+const script = document.createElement('script')
+
+script.src = 'https://udify.app/embed.min.js'
+script.id = 'Biy8n9oNdzKDhB6b'
+script.defer = true
+
+document.body.appendChild(script)
